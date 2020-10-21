348665fb174d7b10a46021895a604f8e39475989
Gustavo Ferreira <gustavofvieira@live.com>
2020-10-21 10:10:31 -0300

    first commit

---
+++ ProjetoModeloDDD.Infra.Data/Contexto/ProjetoModeloContext.cs
@@ -0,0 +1,67 @@
+﻿
+using ProjetoModeloDDD.Domain.Entities;
+using ProjetoModeloDDD.Infra.Data.EntityConfig;
+using System;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Data.SqlClient;
+
+namespace ProjetoModeloDDD.Infra.Data.Contexto
+{
+    public class ProjetoModeloContext : DbContext
+    {
+        public ProjetoModeloContext()
+            : base("ProjetoModeloDDD")
+        {
+
+        }
+
+        //Adicionando DbSet para utilizar o EF em cima dos objetos
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+            //Remove convenções, como Plurais do EF, e deletar em cascata
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+
+            //Onde o nome do atributo + Id vai ser configurado como Chave Primaria
+            modelBuilder.Properties()
+                .Where(p => p.Name == p.ReflectedType.Name + "Id")
+                .Configure(p => p.IsKey());
+
+            //tudo que for string muda para varchar pois o EF seta nvarchar
+            modelBuilder.Properties<string>()
+                .Configure(p => p.HasColumnType("varchar"));
+            //tudo que for string muda o tamanho máximo para 100 pois o EF seta como MAX
+            modelBuilder.Properties<string>()
+                .Configure(p => p.HasMaxLength(100));
+
+            //Adicionando configurações dos models para inserir as alterações
+            modelBuilder.Configurations.Add(new ClienteConfiguration());
+            modelBuilder.Configurations.Add(new ProdutoConfiguration());
+        }
+
+        //Metódo que modifica o tipo para DateTime quando o DataCadastro é adicionado
+        public override int SaveChanges()
+        {
+            foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("DataCadastro") != null))
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Property("DataCadastro").CurrentValue = DateTime.Now;
+                }
+
+                if (entry.State == EntityState.Modified)
+                {
+                    entry.Property("DataCadastro").IsModified = false;
+                }
+              }
+            return base.SaveChanges();
+        }
+    }
+}
---
+++ ProjetoModeloDDD.Infra.Data/Repositories/RepositoryBase.cs
@@ -0,0 +1,50 @@
+﻿
+using System.Collections.Generic;
+using ProjetoModeloDDD.Domain.Interfaces;
+using ProjetoModeloDDD.Infra.Data.Contexto;
+using System.Linq;
+using System.Data.Entity;
+using System.Data.SqlClient;
+
+namespace ProjetoModeloDDD.Infra.Data.Repositories
+{
+    public class RepositoryBase<TEntity> : IDisposible, IRepositoryBase<TEntity> where TEntity : class
+    {
+
+        // adicionando contexto para a manipulação de objetos genéricos
+        protected ProjetoModeloContext Db = new ProjetoModeloContext();
+
+        public void Add(TEntity obj)
+        {
+            Db.Set<TEntity>().Add(obj);
+            Db.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return Db.Set<TEntity>().ToList();
+        }
+
+        public TEntity GetById(int id)
+        {
+            return Db.Set<TEntity>().Find(id);
+        }
+
+        public void Remove(TEntity obj)
+        {
+            Db.Set<TEntity>().Remove(obj);
+            Db.SaveChanges();
+        }
+
+        public void Update(TEntity obj)
+        {
+            Db.Entry(obj).State = EntityState.Modified;
+            Db.SaveChanges();
+        }
+    }
+}
