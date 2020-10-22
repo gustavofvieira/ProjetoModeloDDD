97bd49775853bd4778fa366c844fc61a0ed30a3b
Gustavo Ferreira <gustavofvieira@live.com>
2020-10-22 09:57:31 -0300

    Finalizando projeto

--- ProjetoModeloDDD.MVC/AutoMapper/ViewModelToDomainMappingProfile.cs
+++ ProjetoModeloDDD.MVC/AutoMapper/ViewModelToDomainMappingProfile.cs
@@ -19,6 +19,8 @@ namespace ProjetoModeloDDD.MVC.AutoMapper
             Mapper.CreateMap<Cliente, ClienteViewModel>();
             Mapper.CreateMap<Produto, ProdutoViewModel>();
 
+
+
         }
     }
 }
